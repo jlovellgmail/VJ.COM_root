fba7d97fc3c36d4a3e87284b1355de37d400e9fc
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2016-12-09 16:22:01 -0800

    update nav / header to match preorder site - note
    - it's not filling the full width of the screen
    - console errors about navigation.js
    - do I need to update the font? Because the icons seem to look right
    already

--- incs/nav.php
+++ incs/nav.php
@@ -11,64 +11,121 @@ $login =  $_SESSION["login"];
 $rootpath = $_SERVER['DOCUMENT_ROOT'];
 ?>
 
-<div class="navBgWrapper">
-    <div class='desktop-burger'><i class='icon-menu-1'></i></div>
+<div class="headerContainer">
     <div class="navWidthWrapper">
-        <div class="logoWrapper">
-            <a class="logoLink" href="/"><img class="navLogo" src="/img/vj-logo-white.png" alt="Virgil James" /></a>
-        </div>
-        <div class="navLinksWrapper">
-            <ul class="navLinksList">
-                <li class="aboutLink">
-                    <a class="<?php echo ($page == "about" ? "active" : "") ?>" href="/about.php">About</a>
-                    <i class="icon-angle-down aboutHoverArrow"></i>
-                    <i class="icon-down-dir <?php echo ($page == "about" ? "active" : "") ?>"></i>
-                </li>
-                <li class="lifestyleLink">
-                    <a class="<?php echo ($page == "lifestyle" ? "active" : "") ?>" href="/lifestyle.php">Lifestyle</a>
-                    <i class="icon-angle-down lifestyleHoverArrow"></i>
-                    <i class="icon-down-dir <?php echo ($page == "lifestyle" ? "active" : "") ?>"></i>
-                </li>
-                <li class="collectionsLink">
-                    <a class="<?php echo ($page == "collections" ? "active" : "") ?>" href="/collections.php">Collections</a>
-                    <i class="icon-angle-down collectionsHoverArrow"></i>
-                    <i class="icon-down-dir <?php echo ($page == "collections" ? "active" : "") ?>"></i>
-                </li>
-                <li class="shopLink">
-                    <a class="<?php echo ($page == "shop" ? "active" : "") ?>" href="/shop.php">Shop</a>
-                    <i class="icon-angle-down shopHoverArrow"></i>
-                    <i class="icon-down-dir <?php echo ($page == "shop" ? "active" : "") ?>"></i>
-                </li>
-                <li class="ambassadorsLink">
-                    <a class="<?php echo ($page == "ambassadors" ? "active" : "") ?>" href="/ambassadors.php">Ambassadors</a>
-                    <i class="icon-angle-down ambassadorsHoverArrow"></i>
-                    <i class="icon-down-dir <?php echo ($page == "ambassadors" ? "active" : "") ?>"></i>
-                </li>
-            </ul>
-            <div class="mobileNavFooter footerGrey hide">
-                <ul class="navFooterLinks caps fw-400 f-14px">
-                    <li><a class="caps navFootLink" href='/about.php'>About</a></li>
-                    <li><a class="caps navFootLink" href='/terms.php'>Terms</a></li>
-                    <li><button class="caps navFootLink" onclick="showModal('/incs/modals/common/modalNewsletter.php');">Newsletter</button></li>
-                    <li><button class="caps navFootLink" onclick="showModal('/incs/modals/common/modalContact.php');">Contact</button></li>
-                </ul>
+        <div class="headerHeightWrapper">
+            <div class="logoWrapper">
+                <a class="logoLink" href="preorder/index.php">
+                    
+
+                    <img class="navLogo" src="../img_preorder/VJ_logo.svg" alt="Virgil James" />
+
+
+                </a>
+            </div>
+            <div class="rightItemsContainer">
+                <div class="desktopLinksContainer">
+                    <div class="textLinksContainer">
+
+
+                        <a class="<?php echo ($page == "collections" ? "active" : "") ?>" href="/index.php">Shop</a>
+                        <a class="<?php echo ($page == "about" ? "active" : "") ?>" href="/about.php">About</a>
+
+
+
+
+                    </div>
+                    <a class="iconWrapper" href="javascript:goToCheckout();">
+                        <i class="icon-basket"></i>
+                    </a>           
+                    <a class="iconWrapper" href="preorder/login.php">
+                        <i class="icon-torso"></i>
+                    </a>
+                </div>
+                <div class='burgerContainer'>
+                    <i class="icon-menu visible"></i>
+                    <i class="icon-cancel"></i>
+                </div>
             </div>
         </div>
-        <div class="navIconsWrapper">
-            <div id="CartNavContainer" class="iconWrapper cartIconWrapper cartDropdownWrapper">
-                <?php include $rootpath.'/incs/navCart.php'; ?>
-            </div><div class="iconWrapper userDropdownWrapper">
-                <?php include $rootpath.'/incs/navUser.php'; ?>
+        <div class="dropdown">
+            <div class="dropdownLinksContainer">
+                <div class="textLinksContainer">
+                    <a class="<?php echo ($page == "collections" ? "active" : "") ?>" href="preorder/index.php">Shop</a>
+                    <a class="<?php echo ($page == "about" ? "active" : "") ?>" href="preorder/about.php">About</a>
+
+                    
+                    <!--
+                    <a href="javascript:goToCheckout();">Cart</a>
+                    <a href="preorder/login.php">Account</a>
+                    -->
+                    <a class="iconWrapper" href="javascript:goToCheckout();">
+                        <i class="icon-basket"></i>
+                    </a>           
+                    <a class="iconWrapper" href="preorder/login.php">
+                        <i class="icon-torso"></i>
+                    </a>
+
+
+                </div>
             </div>
         </div>
-        <div class="navBurgerWrapper">
-            <div class="iconWrapper">
-                <button class="burgerButton"><i class="icon-menu-1 navBurgerIcon"></i></button>
-            </div>
+        <script>
+            jQuery(function($){
+
+                //$( '.icon-menu' ).click(function(){
+                $(".burgerContainer").click(function(){
+
+                    $('.dropdown').toggleClass('visible');
+                    $('.burgerContainer .icon-menu').toggleClass("visible");
+                    $(".burgerContainer .icon-cancel").toggleClass("visible");
+
+                });
+            });
+        </script>
+        <script type="text/javascript">
+            var lastScrollTop = 0;
+            var delta = 5;
+            var headerHeight = $(".headerContainer").outerHeight();
+            var didScroll;
+            // on scroll, let the interval function know the user has scrolled
+            $(window).scroll(function(event){
+                didScroll = true;
+            });
+            // run hasScrolled() and reset didScroll status
+            setInterval(function() {
+                if (didScroll) {
+                    hasScrolled();
+                    didScroll = false;
+                }
+            }, 250);
+            function hasScrolled() {
+                var st = $(this).scrollTop();
+                if(Math.abs(lastScrollTop - st) <= delta){
+                    return;
+                }
+                // If current position > last position AND scrolled past navbar...
+                if (st > lastScrollTop && st > headerHeight){
+                    // hide header
+                    $(".headerContainer .dropdown").removeClass("visible");
+                    setTimeout(function() {
+                        $(".headerContainer").addClass("headerUp");
+                    }, 200);
+                    setTimeout(function() {
+                        $('.burgerContainer .icon-menu').addClass("visible");
+                        $(".burgerContainer .icon-cancel").removeClass("visible");
+                    }, 400);
+                } 
+                else {
+                    // show header
+                    if(st + $(window).height() < $(document).height()) { 
+                        $(".headerContainer").removeClass("headerUp");
+                    }
+                }
+                lastScrollTop = st;
+            }
+        </script>
+        <div class="bottomBorder">
         </div>
     </div>
 </div>
-
-<!-- <div class="navPlaceholder"></div> -->
-
-<div class="scrollWaypoint"></div>
